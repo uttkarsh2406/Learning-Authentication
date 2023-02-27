7544ec972cb3c88841be2059a4ce0b09fee9b4f6
uttkarsh2406 <74823692+uttkarsh2406@users.noreply.github.com>
2023-02-27 16:56:56 +0530

    level-3 encryption

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const ejs=require("ejs");
 const app=express();
 const mongoose=require("mongoose");
 const encrypt=require("mongoose-encryption");
-
+const md5=require("md5");
 
 
 app.use(express.static("public"));
@@ -19,7 +19,7 @@ const userSchema=new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt,{secret: process.env.SECRET,encryptedFields: ["password"]});
+// userSchema.plugin(encrypt,{secret: process.env.SECRET,encryptedFields: ["password"]});
 
 const User=new mongoose.model("User",userSchema);
 
@@ -42,7 +42,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newUser=new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err){
         if(err){
@@ -58,7 +58,7 @@ app.post("/register",function(req,res){
 app.post("/login",function(req,res){
     const username=req.body.username;
 
-    const password=req.body.password;
+    const password=md5(req.body.password);
     
     User.findOne({email:username},function(err,foundUser){
         if(err){
